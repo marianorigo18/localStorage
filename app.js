6a590d44e83dee7e8b72a2d59f1372ca4d5565f1
marianorigo18 <marianowagnerrigo@gmail.com>
2023-01-24 11:27:23 -0300

    task

--- app.js
+++ app.js
@@ -1,21 +1,30 @@
 
-localStorage.setItem('saludo', 'Good Morning');
-localStorage.setItem('unNumero', 30);
-localStorage.setItem('esValido', true);
+// localStorage.setItem('saludo', 'Good Morning');
+// localStorage.setItem('unNumero', 30);
+// localStorage.setItem('esValido', true);
 
-sessionStorage.setItem('email', 'mariano@iksfijd.com')
+// sessionStorage.setItem('email', 'mariano@iksfijd.com')
 
-let product1 = {id: 1, name: 'contactor', price: 1500}
+// let product1 = {id: 1, name: 'contactor', price: 1500}
 
-localStorage.setItem('producto1',product1);
+// localStorage.setItem('producto1',product1);
 
-let myProduct = localStorage.getItem('producto1');
+// let myProduct = localStorage.getItem('producto1');
 
-console.log(myProduct)
+// console.log(myProduct)
 
-for(let i = 0; i < localStorage.length; i++){
-    let clave = localStorage.key(i);
-    // console.log(clave)
-    // console.log(localStorage.getItem(clave))
-    console.log(clave, localStorage.getItem(clave), typeof localStorage.getItem(clave))
-}
+// for(let i = 0; i < localStorage.length; i++){
+//     let clave = localStorage.key(i);
+//     console.log(clave)
+//     console.log(localStorage.getItem(clave))
+//     console.log(clave, localStorage.getItem(clave), typeof localStorage.getItem(clave))
+// }
+/*Objet to JSON*/ 
+let product2 = {id: 1, name: 'contactor', price: 1500};
+let miJson = JSON.stringify(product2);
+localStorage.setItem('product2', miJson);
+console.log(miJson)
+console.log(typeof miJson);
+/*JSON to Objet*/
+let getJSON = JSON.parse(localStorage.getItem('product2'));
+console.log(getJSON)
